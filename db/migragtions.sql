c2cbd4384ec33ee6634a5d3adb70af65a1f7ba56
Brian <Brian@Brians-MacBook-Air.local>
2019-05-28 13:05:43 -0500

    a

--- db/migragtions.sql
+++ db/migragtions.sql
@@ -7,20 +7,24 @@ CREATE TABLE users(
     password_digest VARCHAR(60)
 );
 
-CREATE TABLE ingredients(
-    id SERIAL PRIMARY KEY,
-    ingredient_name VARCHAR(255),
-    img_url VARCHAR(255),
-    fridge BOOL
-    user_id INTEGER REFERENCES users(id)
-);
-
-#through table
 CREATE TABLE meals(
     id SERIAL PRIMARY KEY,
     meal_name VARCHAR(255)
-    recipe VARCHAR(255)
-    img_url VARCHAR(255)
-    user_id INTEGER REFERENCES users(id),
+    recipe_link_url VARCHAR(255)
+    meal_img_url VARCHAR(255)
+    user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE ingredients(
+    id SERIAL PRIMARY KEY,
+    ingredient_name VARCHAR(255),
+    ing_img_url VARCHAR(255),
+    meal_id INTEGER REFERENCES meals(id)
+);
+
+CREATE TABLE recipe(
+    id SERIAL PRIMARY KEY,
+    meal_id INTEGER REFERENCES meal(id)
     ingredient_id INTEGER REFERENCES ingredients(id)
+    quantity INTEGER REFERENCES ingredients(id)
 );
