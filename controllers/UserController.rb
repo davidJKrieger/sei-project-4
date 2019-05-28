65d644dada614b75ef101fa85a2bc8dec53b2e94
Brian <Brian@Brians-MacBook-Air.local>
2019-05-28 15:45:27 -0500

    fixed user

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -1,94 +1,119 @@
-class UserAPIController < ApplicationController
+class UserController < ApplicationController
 
-before do
-  if request.post
-    payload_body = request.body.read
-    @payload = JSON.parse(payload_body).symbolize_keys
-    puts "------->   Here is our payload, Json request body"
-    pp @payload
+  # shows login page
+  get '/login' do
+    erb :login
   end
 
-end
-
+  # do the login 
   post '/login' do
+
+    # find user by username
     user = User.find_by username: params[:username]
+
+    # pp user # help us as devs see whether its a username or pw issue
+    # or you could use binding.pry
+
     pw = params[:password]
+
+    # if the user exists and password is correct --
+    # password is now being checked using bcrypt
     if user && user.authenticate(pw)
-    session[:logged_in] = true
-    session[:username] = user.username
-    response = {
-        success: true, 
-        code: 200,
+      # add stuff to session: loggedin, username, message
+      session[:logged_in] = true
+      session[:username] = user.username
+      session[:message] = {
+        success: true,
         status: "good",
-        message: "User #{user.username} successfully logged in." 
+        message: "Logged in as #{user.username}"
       }
-      response.to_json
-      
+      # redirect to /items
+      redirect '/items'
+
+    # else 
     else
-     
-      response = {
+      # error -- incorrect un or pw
+      session[:message] = {
         success: false,
-        code: 200,
         status: "bad",
         message: "Invalid username or password."
       }
-    response.to_json
-
+      
+      # redirect to /login so they can reattempt
+      redirect '/users/login'
     end
 
   end
 
 
- 
+  # shows register 
+  get '/register' do
+    erb :register
+  end
+
+  # do registration
   post '/register' do
 
+    # check if user exists 
     user = User.find_by username: params[:username]
+          # User.find_by({ :username => params[:username] })
+
+    # if user doesn't exist
     if !user
-     user = User.new
-    user.username = params[:username]
-    user.password = params[:password]
+
+      # create user 
+      user = User.new
+      user.username = params[:username]
+
+      # HEY: if has_secure_password is specified in User model
+      # .password= setter method will automatically encrypt
+      # the password for you and store it in a field on the 
+      # User model called "password_digest"
+      user.password = params[:password]
+
       user.save
+
+      # add stuff to session: loggedin, username, message
       session[:logged_in] = true
       session[:username] = user.username
-
-      # API response
-      response = {
+      session[:message] = {
         success: true,
-        code: 201, 
         status: "good",
-        message: "User #{user.username} successfully created"
+        message: "Welcome to the site, you are now logged in as #{user.username}."
       }
 
-      response.to_json
-      
+      # redirect to the site
+      redirect '/items'
+
+    # else if user does exist
     else 
-     
-      response = {
+      # session message -- username taken
+      session[:message] = {
         success: false,
-        code: 200, 
         status: "bad",
         message: "Sorry, username #{params[:username]} is already taken."
       }
+      # redirect to register so they can try again
+      redirect '/users/register'
 
-      response.to_json
-      
     end
 
   end
 
+  # logout
   get '/logout' do
     
-    username = session[:username] 
+    username = session[:username] # grab username before destroying session...
+
     session.destroy
-     response = {
+
+    session[:message] = {
       success: true,
-      code: 200,
       status: "neutral",
-      message: "User #{username} logged out." 
+      message: "User #{username} logged out." #...so that we can use it here
     }
 
-    response.to_json
-
+    redirect '/users/login'
 
   end
 
